5c1db1431fcb6069088ad3c7c7dc7dd3003303d3
lrgsouza <lucas_souza@outlook.com>
2025-03-27 17:06:37 -0300

    update chat id

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const TELEGRAM_BOT_TOKEN = "8016143280:AAHD7ynoMx0Jugj66B6e4h2sWLuruFvBRxI"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "2021046429"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_CHAT_ID = "-4698761899"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
